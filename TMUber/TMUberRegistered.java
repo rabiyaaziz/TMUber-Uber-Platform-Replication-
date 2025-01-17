8e3f57056e33ea50be18fc47b989eeccfb0cc324
rabiyaaziz <148975361+rabiyaaziz@users.noreply.github.com>
2025-01-17 18:46:56 -0500

    Update TMUberRegistered.java

--- TMUber/TMUberRegistered.java
+++ TMUber/TMUberRegistered.java
@@ -1,4 +1,10 @@
+// Rabiya Aziz
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 
 public class TMUberRegistered
 {
@@ -19,34 +25,50 @@ public class TMUberRegistered
         return "" + firstDriverId + current.size();
     }
 
-    // Database of Preregistered users
-    // In Assignment 2 these will be loaded from a file
-    // The test scripts and test outputs included with the skeleton code use these
-    // users and drivers below. You may want to work with these to test your code (i.e. check your output with the
-    // sample output provided). 
-    public static void loadPreregisteredUsers(ArrayList<User> users)
+    public static ArrayList<User> loadPreregisteredUsers(String filename) throws FileNotFoundException
     {
-        users.add(new User(generateUserAccountId(users), "McInerney, T.", "34 5th Street", 25));
-        users.add(new User(generateUserAccountId(users), "Valenzano, R.", "71 1st Street", 55));
-        users.add(new User(generateUserAccountId(users), "Lugez, E.", "55 9th Avenue", 125));
-        users.add(new User(generateUserAccountId(users), "Miranskyy, A.", "15 2nd Avenue", 15));
-        users.add(new User(generateUserAccountId(users), "Raman, P.", "32 3rd Street", 13));
-        users.add(new User(generateUserAccountId(users), "Woungang, I.", "64 6th Avenue", 27));
-        users.add(new User(generateUserAccountId(users), "Soutchanski, M.", "28 7th Avenue", 22));
-        users.add(new User(generateUserAccountId(users), "Harley, E.", "10 7th Avenue", 34));
-        users.add(new User(generateUserAccountId(users), "Mason, D.", "48 8th Street", 11));
-        users.add(new User(generateUserAccountId(users), "Santos, M.", "83 4th Street", 41));
+        // Creates a list of users from a file
+        //Uses a scanner to obtain User info from the file 
+        ArrayList<User> users = new ArrayList<>();
+        File userFile = new File (filename);
+        Scanner scanUser = new Scanner(userFile);
+        while ( scanUser.hasNextLine()) 
+        {
+            String name = scanUser.nextLine();
+            String address = scanUser.nextLine();
+            Double wallet = scanUser.nextDouble();
+            if (scanUser.hasNextLine())
+            {scanUser.nextLine();}
+            users.add(new User(generateUserAccountId(users), name, address, wallet));
+        }
+        scanUser.close();
+        return users;
+     
     }
 
-    // Database of Preregistered users
-    // In Assignment 2 these will be loaded from a file
-    public static void loadPreregisteredDrivers(ArrayList<Driver> drivers)
+   
+    public static ArrayList<Driver> loadPreregisteredDrivers(String filename) throws FileNotFoundException
     {
-        drivers.add(new Driver(generateDriverId(drivers), "Tom Cruise", "Toyota Corolla", "MAVERICK"));
-        drivers.add(new Driver(generateDriverId(drivers), "Brad Pitt",  "Audi S4", "FGDR 983"));
-        drivers.add(new Driver(generateDriverId(drivers), "Millie Brown",  "Tesla", "STRNGRTHGS"));
-        drivers.add(new Driver(generateDriverId(drivers), "Tim Chalamet",  "Thopter", "DUNE"));
-        drivers.add(new Driver(generateDriverId(drivers), "John Boyega",  "X-Wing", "REBEL"));
+        /* Creates a list of drivers from a file
+           Uses a scanner to obtain Driver info from the file 
+         */ 
+        
+
+        ArrayList<Driver> drivers = new ArrayList<>();
+        File driverFile = new File (filename);
+        Scanner scanDriver = new Scanner(driverFile);
+        while ( scanDriver.hasNextLine()) 
+        {
+
+            String name = scanDriver.nextLine();
+            String carmodel = scanDriver.nextLine();
+            String carlicense = scanDriver.nextLine();
+            String address = scanDriver.nextLine();
+            drivers.add(new Driver(generateDriverId(drivers), name,  carmodel, carlicense,address));
+        }
+        scanDriver.close();
+        return drivers;
+    
     }
 }
 
